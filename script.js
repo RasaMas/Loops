4c6d9761263c3e64ef63be086d13965cffc8a937
RasaMas <144053645+RasaMas@users.noreply.github.com>
2023-10-03 16:34:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+//function count(num) {
+//    console.log('- ' + num + ' !!!')
+//}
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log('- ' + i + ' !!!')
+// }
+
+function count(num) {
+    console.log('- ' + num + ' !!!')
+}
+
+// function task() {
+//     for (let i = 1; i <= 100; i++) {
+//         console.log(i)
+//     }
+// }
+
+function task1() {
+    for (let i = 1; i <= 100; i++) {
+        let answer = 1 * 2
+        let output = `${i} * 2 = ${answer}`
+        console.log(output)
+    }
+}
+
+function task2() {
+    for (let i = 1; i <= 100; i++) {
+        let answer = i * 5
+        let output = `${i} * 5 = $[answer]`
+        console.log(output)
+    }
+}
+
+function task3() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i + 5)
+    }
+}
+
+function task4() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i - 2)
+    }
+}
+
+function task5() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i * i)
+    }
+}
+
+function task6() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i**3)
+    }
+}
+
+function task71 () {
+    for (let i = 100; i >= 1; i--) {
+        console.log(1 * 2)
+    }
+}
